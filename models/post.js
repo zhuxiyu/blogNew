9205e949de452e38d8ca36aab075b84023b40434
zhuxiyu <zhuxiyu2012@gmail.com>
2016-12-07 18:56:08 +0800

    分页等

--- models/post.js
+++ models/post.js
@@ -22,7 +22,8 @@ Post.prototype.save = function(callback) {
 		name:this.name,
 		time:time,
 		title:this.title,
-		post:this.post
+		post:this.post,
+		comments:[]
 	}
 
 	//打开数据库
@@ -50,37 +51,43 @@ Post.prototype.save = function(callback) {
 	})
 };
 
-//读取文章及其相关信息
-Post.getAll = function(name,callback){
+//一次获取十篇文章
+Post.getTen = function(name, page, callback){
 	//打开数据库
-	mongodb.open(function(err,db){
+	mongodb.open(function(err, db){
 		if(err){
 			return callback(err);
 		}
-		//读取posts集合
-		db.collection('posts',function(err,collection){
+		//读取 posts集合
+		db.collection('posts', function(err, collection){
 			if(err){
 				mongodb.close();
 				return callback(err);
 			}
 			var query = {};
 			if(name){
-				query.name = name;
+				query.name = name ;
 			}
-			//根据query对象查询
-			collection.find(query).sort({
-				time:-1
-			}).toArray(function(err,docs){
-				mongodb.close();
-				if(err){
-					return callback(err);//失败！返回err
-				}
-				//解析 markdown 为 html
-				docs.forEach(function(doc){
-					doc.post = markdown.toHTML(doc.post);
-				})
-				callback(null,docs);//成功!以数组形式返回查询的结果
-			});
+			//使用 count 返回特定查询的文档数 total
+			collectionc.count(query, function(err, total){
+				//根据query 对象查询，并跳过前(pagr-1) *10个结果，返回之后的10个结果
+				collection.find(query, {
+					skip: (page - 1)*10,
+					limit : 10
+				}).sort({
+					time:-1
+				}).toArray(function(err, docs){
+					mongodb.close();
+					if(err){
+						return callback(err);
+					};
+					//解析 markdown 为 html
+					docs.forEach(function(doc){
+						doc.post = markdown.toHTML(doc.post);
+					});
+					callback(null, docs, total);
+				});
+			})
 		})
 	})
 }
@@ -109,7 +116,12 @@ Post.getOne = function(name,day,title,callback){
 					return callback(err);
 				}
 				//解析markdown为html
-				doc.post = markdown.toHTML(doc.post);
+				if(doc){
+					doc.post = markdown.toHTML(doc.post);
+					doc.comments.forEach(function(comment){
+						comment.content = markdown.toHTML(comment.content);
+					})
+				}
 				callback(null,doc);//返回查询的文章
 			})
 		})
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var crypto = require('crypto'),
     User = require('../models/user.js'),
     Post = require('../models/post.js'),
+    Comment = require('../models/comment.js'),
     multer = require('multer'); //图片上传
 var storage = multer.diskStorage({
       destination: function (req, file, cb) {
@@ -13,17 +14,23 @@ var storage = multer.diskStorage({
 var upload = multer({ storage: storage });
 module.exports = function (app) {
   app.get('/',function (req,res) {
-    Post.getAll(null,function(err,posts){
+    //判断是否是第一页，并把请求的页数转成 number 类型
+    var page = req.query.p ? parseInt(req.query.p):1;
+    //查询并返回第 page 页的 10 篇文章
+    Post.getTen(null, page, function(err, posts, total){
       if(err){
         posts = [];
-      }
+      };
       res.render('index',{
         title:'主页',
-        user: req.session.user,
         posts:posts,
+        page:page,
+        isFirstPage:(page - 1) == 0,
+        isLastPage :((page -1) *10 + posts.length) == total,
+        user:req.session.user,
         success:req.flash('success').toString(),
         error:req.flash('error').toString()
-      });
+      })
     })
   });
 
@@ -163,14 +170,15 @@ module.exports = function (app) {
   });
 
   app.get('/u/:name',function(req,res){
+    var page = req.query.p ? parseInt(req.query.p):1;
     //检查用户是否存在
     User.get(req.params.name,function(err,user){
       if(!user){
         req.flash('error','用户不存在！');
         return res.redirect('/');//用户不存在则跳转到主页
       }
-      //查询并返回该用户的所有文章
-      Post.getAll(user.name,function(err,posts){
+      //查询并返回该用户第 page 页的 10 篇文章
+      Post.getTen(user.name,page,function(err,posts,total){
         if(err){
           req.flash('error',err);
           return res.redirect('/');
@@ -178,6 +186,9 @@ module.exports = function (app) {
         res.render('user',{
           title:user.name,
           posts:posts,
+          page:page,
+          isFirstPage:(page - 1) == 0,
+          isLastPage :((page -1) *10 + posts.length) == total,
           user:req.session.user,
           success:req.flash('success').toString(),
           error:req.flash('error').toString()
@@ -262,4 +273,26 @@ module.exports = function (app) {
       res.redirect('/'); //成功！返回文章页
     })
   });
+
+  app.post('/u/:name/:day/:title', function(req, res){
+    var date = new Date(),
+      time = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + date.getHours() + ":" + (date.getMinutes() < 10? "0" + date.getMinutes() : date.getMinutes());
+      console.log(req.body.website,"999899890890890998");
+    var comment = {
+      name:req.body.name,
+      email:req.body.email,
+      website:req.body.website,
+      time:time,
+      content:req.body.content
+    };
+    var newComment = new Comment(req.params.name, req.params.day, req.params.title, comment);
+    newComment.save(function(err){
+      if(err){
+        req.flash('error',err);
+        return res.redirect('back');
+      }
+      req.flash('success','留言成功!');
+      res.redirect('back');
+    })
+  })
 };
